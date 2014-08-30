65ef4cd020062a620cd139b3f537f66de9ef51c0
Ryan Rose <rytrose@gmail.com>
2014-08-30 13:32:36 -0400

    Initial commit. Constructors and one field.

---
+++ P0/src/Annotation.java
@@ -0,0 +1,25 @@
+/**
+ * Annotates a number in accordance to the rules of the "FizzBuzz" game.
+ * EECS 233
+ * @author Ryan Rose
+ */
+
+public class Annotation {
+	/**
+	 * The integer to be annotated.
+	 */
+	private int n = 0;
+	
+	/**
+	 * Constructor that sets n to a default of 0. 
+	 */
+	public Annotation(){
+	}
+	
+	/**
+	 * Constructor with an integer parameter sets n to the parameter value.
+	 */
+	public Annotation(int i){
+		this.n = i;
+	}
+}
