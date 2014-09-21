68ac2903b3a2281fa9b925b56dc0065a5806addb
Ryan Rose <rytrose@gmail.com>
2014-09-21 17:35:47 -0400

    chose to use a NumArrayList as a field

--- P1/src/NumSet.java
+++ P1/src/NumSet.java
@@ -3,6 +3,40 @@
  * EECS 233, Prof. Lewicki
  * @author Ryan Rose
  */
-public class NumSet {
-
+public class NumSet{
+	/**
+	 * NumArrayList that stores the list data.
+	 */
+	private NumArrayList list;
+	
+	/**
+	 * Constructor that takes an array as the set values.
+	 * @param array array parameter that defines the set
+	 */
+	public NumSet(double[] array){
+		for(int i = 0; i < array.length; i++){
+			list.add(array[i]);
+		}
+		list.removeDuplicates();
+	}
+	
+	/**
+	 * Returns the NumArrayList that is storing the set contents.
+	 * @return the NumArrayList that is storing the set contents
+	 */
+	private NumArrayList getList(){
+		return list;
+	}
+	
+	/**
+	 * Returns the size of the set.
+	 */
+	public int size(){
+		return getList().size();
+	}
+	
+	
+	
+	
+	
 }
